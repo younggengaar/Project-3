40b22e960ca0835fcf4aefaaf0312622c7b88b57
liza <123323177+younggengaar@users.noreply.github.com>
2023-02-13 23:30:02 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Размер массива: ");
+            //byte size = Convert.ToByte(Console.ReadLine());
+            //int[] mass = new int[size];
+            //Random rand = new Random();
+            //int i;
+            //for (i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = rand.Next(-10, 10);
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+            //int d = 0;
+            ////for (i = 0; i < mass.Length; i++)
+            ////{
+            ////    if (mass[i] > 0) d++;
+            ////}
+            //foreach (int elem in mass)
+            //{
+            //    if (elem > 0) d++;
+            //}
+            //Console.WriteLine($"d={d}");
+            //Console.ReadLine();
+
+            //Console.Write("Размер массива: ");
+            //byte size = Convert.ToByte(Console.ReadLine());
+            //int[] mass = new int[size];
+            //Random rand = new Random();
+            //int i;
+            //for (i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = rand.Next(-10, 10);
+            //}
+            //for (i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] *= 2;
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
